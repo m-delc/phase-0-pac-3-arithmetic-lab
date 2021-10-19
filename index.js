6a5f167630939d59f0911607b65bcfb4c7e283ad
MichaelDelcambre <delcambrem@gmail.com>
2021-10-18 20:09:34 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(num1, num2) {
+    let res = num1 + num2
+    return res
+}
+function subtract(num1, num2) {
+    let res = num1 - num2
+    return res
+}
+function multiply(num1, num2) {
+    let res = num1 * num2
+    return res
+}
+function divide(num1, num2) {
+    let res = num1 / num2
+    return res
+}
+function increment (n) {
+    return (n += 1);
+}
+function decrement (n) {
+    return (n -= 1);
+}
+const n = 2.222
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
